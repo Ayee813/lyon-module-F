88fd57318ac21ce6d209c33d462df5e27bc16599
ayee813 <yongyeelove813@gmail.com>
2025-06-07 19:37:53 +0700

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+This is one of 7 module in worldskill 2024, This module is about Lyon tourist page.
+Here is how you can run:
++ cd to your project file, for me: cd ~/Desktop/Lyon_Module_complete/Module_F_lyon_tourist_page
+#bash 
+python3 -m http.server 8080
+
+then go to your browser with URL: localhost:8080
+
+
+//this web page has offline mode you can use it through dev too --> application --> looking for 
--- js/Script.js
+++ js/Script.js
@@ -24,12 +24,52 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-function tabLoad(tab) {
+//for loading tab index
+function tabLoad(tab, button) {
     fetch(tab + '.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('content').innerHTML = data;
-            setActiveMenuItem(tab);
+            document.getElementById('tab').innerHTML = data;
+
+            // Remove 'active' class from all buttons
+            const buttons = document.querySelectorAll('.tab-index button');
+            buttons.forEach(btn => btn.classList.remove('active'));
+
+            // Add 'active' class to the clicked button
+            if (button) button.classList.add('active');
         })
         .catch(error => console.log('Error loading page:', error));
 }
+
+
+//for load tab1 is the default content
+window.onload = function () {
+    const firstButton = document.querySelector('.tab-index button');
+    tabLoad('tabPage/tab1', firstButton);
+};
+
+function readText() {
+    const infoSection = document.querySelector('.info-left');
+    const text = infoSection.textContent;
+    const speech = new SpeechSynthesisUtterance(text);
+    speech.lang = "fr-FR";
+    speechSynthesis.speak(speech);
+}
+
+function overlayAdd(el) {
+    const overlay = el.querySelector('.overlay');
+    overlay.classList.add('overlay-hover');
+}
+
+function overlayRemove(el) {
+    const overlay = el.querySelector('.overlay');
+    overlay.classList.remove('overlay-hover');
+}
+
+// function btnEffcet(){
+//     const button = document.querySelector('.call-btn');
+
+//     button.addEventListener("mousemove", function(e){
+//         const x = e.clientX
+//     })
+// }
